c5a0390ae01920a6554caed4c4067169a0c573b2
HSZJS <heshuzhan@heshuzhandeMacBook-Pro.local>
2016-07-25 15:53:31 +0800

    1
    1

--- pod725/Pods/Target Support Files/Pods-pod725/Pods-pod725-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_pod725 : NSObject
-@end
-@implementation PodsDummy_Pods_pod725
-@end
--- pod725/Pods/Target Support Files/Pods-pod725UITests/Pods-pod725UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_pod725UITests : NSObject
-@end
-@implementation PodsDummy_Pods_pod725UITests
-@end
